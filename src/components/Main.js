c44462c2f844beadb7588ce6a4b40c142f6ba73f
yifan <yifan@magicmoma.com>
2017-07-26 18:02:19 +0800

    添加图片 基本样式

--- src/components/Main.js
+++ src/components/Main.js
@@ -22,12 +22,12 @@ imageDatas = (function getImageURL(imageDatasArr) {
 class ImgFigure extends React.Component {
     render() {
         return (
-            <figure>
+            <figure className="img-figure">
                 <img src={this.props.data.imageURL}
                     alt={this.props.data.title}
                 />
                 <figcaption>
-                    <h2>{this.props.data.title}</h2>
+                    <h2 className="img-title">{this.props.data.title}</h2>
                 </figcaption>
             </figure>
         );
