74e86dbdb9831a42bcad4ad6107d7bb4627996f2
yifan <yifan@magicmoma.com>
2017-07-26 14:26:58 +0800

    finish 3-1 add images, stage

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,32 @@
 require('normalize.css/normalize.css');
 require('styles/App.css');
+require('styles/main.scss');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+// 获取图片相关的数据
+var imageDatas = require('../sources/imageDatas.json');
+
+// 利用自执行函数，将图片名信息转成图片URL路径信息
+imageDatas = (function getImageURL(imageDatasArr) {
+    for (var i = 0; i < imageDatasArr.length; i++) {
+        var singleImageData = imageDatasArr[i];
+        singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+        imageDatasArr[i] = singleImageData;
+    }
+
+    return imageDatasArr;
+})(imageDatas);
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+        <section className="stage">
+             <section className="img-sec">
+             </section>
+             <nav className="controller-nav">
+             </nav>
+         </section>
     );
   }
 }
